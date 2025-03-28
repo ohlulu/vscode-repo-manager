d1c179f0ceaf2ec0d501e854e3ee382ec6db6963
Ohlulu Hsih <z30262226@gmail.com>
2025-03-28 16:34:58 +0800

    建立擴展激活機制

--- src/extension.js
+++ src/extension.js
@@ -3,12 +3,21 @@
  * @module extension
  */
 
+import * as vscode from 'vscode';
+
 /**
  * Activates the extension
  * @param {vscode.ExtensionContext} context - The extension context
  */
 export function activate(context) {
   console.log('Repository Manager extension is now active');
+  
+  // Register commands
+  const disposable = vscode.commands.registerCommand('repoManager.showRepoView', () => {
+    vscode.window.showInformationMessage('Repository Manager view is activated!');
+  });
+  
+  context.subscriptions.push(disposable);
 }
 
 /**
